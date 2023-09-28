bd42369cff4e93d08113b23410e50d75079d6ef2
Nikkgt <seglog15200@gmail.com>
2023-09-29 02:02:25 +0300

    new method number added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(5+5);
+        System.out.println(5+5*10);
+        int oneHundred = newMethod(1);
+    }
+    public static int newMethod(int number){
+        while (number<=100){
+            number++;
+        }
+        return number;
     }
 }
